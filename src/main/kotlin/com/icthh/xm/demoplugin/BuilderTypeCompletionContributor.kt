d88bdbed8bcb656bc60cda05712c9c54f9e62fcd
Serhii Senko <serhii.senko@jevera.software>
2021-03-14 18:52:53 +0200

    Implement builder type completion

--- src/main/kotlin/com/icthh/xm/demoplugin/BuilderTypeCompletionContributor.kt
+++ src/main/kotlin/com/icthh/xm/demoplugin/BuilderTypeCompletionContributor.kt
@@ -4,36 +4,23 @@ import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.patterns.PsiElementPattern
 import com.intellij.psi.PsiElement
 import com.intellij.util.ProcessingContext
+import org.jetbrains.annotations.NotNull
 import org.jetbrains.yaml.psi.*
 import org.jetbrains.yaml.psi.impl.YAMLSequenceImpl
 
-class BuilderTypeCompletionContributor: CompletionContributor() {
+class BuilderTypeCompletionContributor : CompletionContributor() {
 
     init {
-        extend(
-            CompletionType.BASIC,
-            PlatformPatterns.psiElement().withParent(
-                psiElement<YAMLScalar>().withParent(
-                    psiElement<YAMLKeyValue>().withName("builderType").withParent(
-                        psiElement<YAMLMapping>().withParent(
-                            psiElement<YAMLSequenceItem>().withParent(
-                                psiElement<YAMLSequence>().withParent(
-                                    psiElement<YAMLKeyValue>().withName("links")
-                                )
-                            )
-                        )
-                    )
-                )
-            ),
-            toResult(listOf("NEW", "SEARCH"))
-        )
+        extend(CompletionType.BASIC, getPattern(), toResult(listOf("NEW", "SEARCH")))
     }
+
 
     private fun toResult(variants: List<String>) = object : CompletionProvider<CompletionParameters>() {
         override fun addCompletions(
-            parameters: CompletionParameters,
+            params: CompletionParameters,
             context: ProcessingContext,
             result: CompletionResultSet
         ) {
@@ -42,6 +29,20 @@ class BuilderTypeCompletionContributor: CompletionContributor() {
         }
     }
 
-    inline fun <reified T: PsiElement> psiElement() = psiElement(T::class.java)
-
 }
+
+fun getPattern() = PlatformPatterns.psiElement().withParent(
+    psiElement<YAMLScalar>().withParent(
+        psiElement<YAMLKeyValue>().withName("builderType").withParent(
+            psiElement<YAMLMapping>().withParent(
+                psiElement<YAMLSequenceItem>().withParent(
+                    psiElement<YAMLSequence>().withParent(
+                        psiElement<YAMLKeyValue>().withName("links")
+                    )
+                )
+            )
+        )
+    )
+)
+
+inline fun <reified T : PsiElement> psiElement() = psiElement(T::class.java)
