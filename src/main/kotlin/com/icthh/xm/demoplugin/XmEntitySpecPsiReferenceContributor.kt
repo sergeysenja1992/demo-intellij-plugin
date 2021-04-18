5df1a3ee7c321786d66dd968a15c648d734701a5
Serhii Senko <serhii.senko@jevera.software>
2021-04-18 19:02:52 +0300

    Cleanup code

--- src/main/kotlin/com/icthh/xm/demoplugin/XmEntitySpecPsiReferenceContributor.kt
+++ src/main/kotlin/com/icthh/xm/demoplugin/XmEntitySpecPsiReferenceContributor.kt
@@ -1,10 +1,7 @@
 package com.icthh.xm.demoplugin
 
-import com.intellij.codeInsight.daemon.EmptyResolveMessageProvider
 import com.intellij.psi.*
-import com.intellij.psi.PsiReferenceRegistrar.HIGHER_PRIORITY
 import com.intellij.psi.impl.RenameableFakePsiElement
-import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.GlobalSearchScope.FilesScope
 import com.intellij.psi.util.descendants
 import com.intellij.psi.util.parentOfType
@@ -14,54 +11,27 @@ import org.jetbrains.yaml.psi.YAMLKeyValue
 import org.jetbrains.yaml.psi.YAMLSequence
 import org.jetbrains.yaml.psi.YAMLValue
 
-
 class XmEntitySpecPsiReferenceContributor: PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         registrar.registerReferenceProvider(
             scalarPattern("typeKey"),
-            getReferenceProvider(),
-            HIGHER_PRIORITY
-        )
-        registrar.registerReferenceProvider(
-            keyPattern(),
-            object : PsiReferenceProvider() {
-                override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-                    return arrayOf(LinkTypeKeyReference(element, context))
-                }
-            },
-            HIGHER_PRIORITY
+            getReferenceProvider()
         )
     }
 
     private fun getReferenceProvider() = object : PsiReferenceProvider() {
         override fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-            return arrayOf(LinkTypeKeyReference(element, context))
+            return arrayOf(LinkTypeKeyReference(element))
         }
     }
 }
 
-class LinkTypeKeyReference(val parent: PsiElement, val context: ProcessingContext):
-    PsiReferenceBase<PsiElement>(parent, false),
-    EmptyResolveMessageProvider {
-
-    private fun findReferenceTarget(element: PsiElement): YAMLValue? {
-        return getTypeKeys(element).firstOrNull { it.valueText == element.text }?.value
-    }
+class LinkTypeKeyReference(val parent: PsiElement): PsiReferenceBase<PsiElement>(parent, false) {
 
     override fun resolve() = findReferenceTarget(parent)?.let{ YAMLNamedPsiScalar(it) }
-    override fun getUnresolvedMessagePattern(): String = "Entity with typeKey ${parent.text} not found"
 
-    override fun getVariants(): Array<Any> {
-        return getTypeKeys(element).map { it.valueText }.toTypedArray()
-    }
-
-    override fun isReferenceTo(element: PsiElement): Boolean {
-        val target = findReferenceTarget(parent)
-        if (element is YAMLNamedPsiScalar) {
-            return element.source == target
-        }
-        return element == target
-    }
+    private fun findReferenceTarget(element: PsiElement) =
+        getTypeKeys(element).firstOrNull { it.valueText == element.text }?.value
 
     private fun getTypeKeys(element: PsiElement): List<YAMLKeyValue> {
         val yamlDocument = element.parentOfType<YAMLDocument>() ?: return emptyList()
@@ -70,43 +40,36 @@ class LinkTypeKeyReference(val parent: PsiElement, val context: ProcessingContex
         }.toList()
     }
 
-    private inline fun <reified T: PsiElement> PsiElement.getChildrenOfType() = this.descendants(canGoInside = {
-        it !is T
-    }).filterIsInstance<T>()
+    private inline fun <reified T: PsiElement> PsiElement.getChildrenOfType() =
+        this.descendants(canGoInside = { it !is T }).filterIsInstance<T>()
+
+    override fun getVariants() = getTypeKeys(element).map { it.valueText }.toTypedArray()
+
+    override fun isReferenceTo(element: PsiElement): Boolean {
+        val target = findReferenceTarget(parent)
+        if (element is YAMLNamedPsiScalar) {
+            return element.source == target
+        }
+        return element == target
+    }
 }
 
 class YAMLNamedPsiScalar(val source: YAMLValue): RenameableFakePsiElement(source) {
 
     val file = source.containingFile.virtualFile
 
-    override fun getName(): String {
-        return ElementManipulators.getValueText(source)
-    }
+    override fun getName() = ElementManipulators.getValueText(source)
 
     override fun getTypeName() = "Target entity type key"
 
     override fun getIcon() = null
 
-    override fun getNavigationElement(): PsiElement {
-        return source
-    }
+    override fun getNavigationElement() = source
 
-    override fun canNavigate(): Boolean {
-        return true
-    }
+    //override fun setName(name: String) = ElementManipulators.handleContentChange(source, name)
 
-    override fun setName(name: String): PsiElement {
-        ElementManipulators.handleContentChange(source, name)
-        return this
-    }
+    override fun getResolveScope() = FilesScope.filesScope(project, listOf(file))
 
-    override fun getResolveScope(): GlobalSearchScope {
-        return FilesScope.filesScope(project, listOf(file))
-    }
-
-    override fun getUseScope(): GlobalSearchScope {
-        return FilesScope.filesScope(project, listOf(file))
-    }
+    override fun getUseScope() = FilesScope.filesScope(project, listOf(file))
 
 }
-
