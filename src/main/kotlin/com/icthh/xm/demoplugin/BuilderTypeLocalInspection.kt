c3b9261de17ce6033c4b1dd5e2e0d0b46d194593
Serhii Senko <serhii.senko@jevera.software>
2021-05-16 18:31:31 +0300

    add qick fix

--- src/main/kotlin/com/icthh/xm/demoplugin/BuilderTypeLocalInspection.kt
+++ src/main/kotlin/com/icthh/xm/demoplugin/BuilderTypeLocalInspection.kt
@@ -4,8 +4,6 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
-import com.jetbrains.rd.util.first
-import org.apache.commons.text.similarity.LevenshteinDistance
 import org.jetbrains.yaml.psi.YamlPsiElementVisitor
 
 class BuilderTypeLocalInspection : LocalInspectionTool() {
@@ -19,11 +17,6 @@ class BuilderTypeLocalInspection : LocalInspectionTool() {
 
             override fun visitElement(element: PsiElement) {
                 if (getPattern().accepts(element) && !acceptableValue.contains(element.text)) {
-
-                    val levenshtein = LevenshteinDistance()
-                    val variants = acceptableValue.map { it to levenshtein.apply(it, element.text) }.toMap()
-                    val key = variants.minByOrNull { it.value }?.key ?: variants.first().key
-
                     holder.registerProblem(element, "${element.text} is invalid builder type")
                 }
             }
